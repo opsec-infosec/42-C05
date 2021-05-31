8d0da0c5793664e67d78c9e33d41d9e59fcc7aa6
Dale Furneaux <dfurneau@lab1r1s12.42abudhabi.ae>
2021-05-31 23:42:35 +0400

    Fixed ex07 0,-5,1 should return 2

--- ex07.c
+++ ex07.c
@@ -6,14 +6,14 @@ int ft_find_next_prime(int nb);
 int	main(void)
 {
 	printf("\nCalling ft_is_prime(int nb);\n\n");
-	printf("nb = 0		 | ret = %d		| Expected = 0\n",ft_find_next_prime(0));
-	printf("nb = -5		 | ret = %d		| Expected = 0\n",ft_find_next_prime(-5));
+	printf("nb = 0		 | ret = %d		| Expected = 2\n",ft_find_next_prime(0));
+	printf("nb = -5		 | ret = %d		| Expected = 2\n",ft_find_next_prime(-5));
 	printf("nb = 1		 | ret = %d		| Expected = 2\n",ft_find_next_prime(1));
 	printf("nb = 2		 | ret = %d		| Expected = 2\n",ft_find_next_prime(2));
 	printf("nb = 8		 | ret = %d		| Expected = 11\n",ft_find_next_prime(8));
 	printf("nb = 20		 | ret = %d		| Expected = 23\n",ft_find_next_prime(20));
 	printf("nb = 42		 | ret = %d		| Expected = 43\n",ft_find_next_prime(42));
-	printf("nb = 2147483630		 | ret = %d		| Expected = 2147483647\n",ft_find_next_prime(2147483630));
+	printf("nb = 2147483630		| ret = %d	| Expected = 2147483647\n",ft_find_next_prime(2147483630));
 	
 	return (0);
 }
