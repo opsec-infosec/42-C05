de1ebbe5dc00ae823ab2e22a422dccb8180b6416
Dale Furneaux <dfurneau@lab1r1s12.42abudhabi.ae>
2021-06-01 16:38:21 +0400

    Added timeout and additional check to ex05

--- ex05.c
+++ ex05.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
 #include "../ex05/ft_sqrt.c"
 
 int ft_sqrt(int nb);
 
-int	main(void)
+int func_done = 0;
+int timer_done = 0;
+
+void *timer(void *var)
 {
+	var = 0x00;
+	sleep(5);
+	timer_done = 1;
+	return (0);
+}
+
+void *execute(void *var)
+{
+	var = 0x00;
 	printf("\nCalling ft_sqrt(int nb);\n\n");
 	printf("nb = 0		 | ret = %d		| Expected = 0\n",ft_sqrt(0));
 	printf("nb = -3		 | ret = %d		| Expected = 0\n",ft_sqrt(-3));
-	printf("nb = 3		 | ret = %d		| Expected = 0\n",ft_sqrt(3));
-	printf("nb = 81		 | ret = %d		| Expected = 9\n",ft_sqrt(81));
-	printf("nb = 729		 | ret = %d		| Expected = 27\n",ft_sqrt(729));
-	printf("nb = 2147483647	 | ret = %d		| Expected = 0\n",ft_sqrt(2147483647));
-	printf("nb = -2147483648	 | ret = %d		| Expected = 0\n",ft_sqrt(-2147483648));
-	
+	printf("nb = 1		 | ret = %d		| Expected = 1\n",ft_sqrt(1));
+	printf("nb = 2		 | ret = %d		| Expected = 0\n",ft_sqrt(2));
+	printf("nb = 2147483646	 | ret = %d		| Expected = 0\n",ft_sqrt(2147483646));
+	printf("nb = 30591961	 | ret = %d		| Expected = 5531\n",ft_sqrt(30591961));
+	printf("nb = 256128016 	 | ret = %d		| Expected = 16004\n",ft_sqrt(256128016));
+	printf("nb = 267747769	 | ret = %d		| Expected = 16363\n",ft_sqrt(267747769));
+	printf("nb = 2147395600	 | ret = %d		| Expected = 46340\n",ft_sqrt(2147395600));
+	func_done = 1;
 	return (0);
 }
+
+int	main(void)
+{
+	pthread_t threadId;
+	pthread_t timerId;
+	int	threadId_Sig;
+	
+	threadId_Sig = pthread_create(&threadId, NULL, execute,  NULL);
+	pthread_create(&timerId, NULL, timer, NULL);
+
+	while (1) {
+		if (timer_done) {
+			pthread_cancel(threadId);
+			printf("*** TIMEOUT ***\n");
+			return (1);
+		}
+		if (func_done)
+			return (0);
+	}
+	return (0);
+}
+
