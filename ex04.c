47e13454fe0cc6e130d8e81d84872c39f1ecabb9
Dale Furneaux <dfurneau@lab1r1s12.42abudhabi.ae>
2021-06-01 23:05:38 +0400

    fix ex04 to use -743

--- ex04.c
+++ ex04.c
@@ -22,7 +22,7 @@ void *execute(void *var)
 	var = 0x00;
 	printf("\nCalling ft_fibonacci(int index);\n\n");
 	printf("index = 0	 | ret = %d		| Expected = 0\n",ft_fibonacci(0));
-	printf("index = -743	 | ret = %d		| Expected = -1\n",ft_fibonacci(-1));
+	printf("index = -743	 | ret = %d		| Expected = -1\n",ft_fibonacci(-743));
 	printf("index = 2	 | ret = %d		| Expected = 1\n",ft_fibonacci(2));
 	printf("index = 3	 | ret = %d		| Expected = 2\n",ft_fibonacci(3));
 	printf("index = 9	 | ret = %d		| Expected = 34\n",ft_fibonacci(9));
