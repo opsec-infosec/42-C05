d85085ccc918173ff7c07fd4ef100b6778514f61
Dale Furneaux <dfurneau@lab1r1s12.42abudhabi.ae>
2021-06-02 12:00:20 +0400

    ex06, added composite numbers 6,8,9

--- ex06.c
+++ ex06.c
@@ -12,7 +12,7 @@ int timer_done = 0;
 void *timer(void *var)
 {
 	var = 0x00;
-	sleep(5);
+	sleep(10);
 	timer_done = 1;
 	return (0);
 }
@@ -24,11 +24,14 @@ void *execute(void *var)
 	printf("nb = 0		 | ret = %d		| Expected = 0\n",ft_is_prime(0));
 	printf("nb = -5		 | ret = %d		| Expected = 0\n",ft_is_prime(-5));
 	printf("nb = 1		 | ret = %d		| Expected = 0\n",ft_is_prime(1));
+	printf("nb = 6		 | ret = %d		| Expected = 0\n",ft_is_prime(6));
+	printf("nb = 8		 | ret = %d		| Expected = 0\n",ft_is_prime(8));
+	printf("nb = 9		 | ret = %d		| Expected = 0\n",ft_is_prime(9));
 	printf("nb = 2		 | ret = %d		| Expected = 1\n",ft_is_prime(2));
 	printf("nb = 7		 | ret = %d		| Expected = 1\n",ft_is_prime(7));
 	printf("nb = 19		 | ret = %d		| Expected = 1\n",ft_is_prime(19));
 	printf("nb = 23		 | ret = %d		| Expected = 1\n",ft_is_prime(23));
-	printf("nb = 2147483647		 | ret = %d		| Expected = 1\n",ft_is_prime(2147483647));
+	printf("nb = 2147483647	 | ret = %d		| Expected = 1\n",ft_is_prime(2147483647));
 	func_done = 1;
 	return (0);
 }
